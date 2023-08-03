765d5b290f17909c990b470e6ac3463426e42140
Sxamoecode <samuelayandare@gmail.com>
2023-08-03 02:08:32 -0800

    Done

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -4,7 +4,14 @@ Encryption main file
 """
 
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
+
 
 password = "MyAmazingPassw0rd"
 print(hash_password(password))
 print(hash_password(password))
+
+print('\n')
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
